f959bf530c65154b34d43b0f3aea0d25506d7800
Rf2025 <165812829+Rf2025@users.noreply.github.com>
2024-09-05 22:14:08 -0400

    Created routes for user CRUD OPS

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const app = express();
 // set up cors to work with front end and back end, also allows CRUD ops
 // origin: ['https://main.d2m4jxyp4by48k.amplifyapp.com']
 const corsOptions = {
-    origin: ['https://main.d2m4jxyp4by48k.amplifyapp.com'], 
+    origin: ['http://localhost:5174'], 
     methods: 'GET,POST,PUT,DELETE',
     credentials: true,
 };
